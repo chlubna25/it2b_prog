dcaf7696e0872c8180bf3c54f68a0b39c5f36cee
MatejKudla <matejkudla@users.noreply.github.com>
2026-04-23 09:17:08 +0200

    OOP

---
+++ projekty/C#/OOP/Studenti/Student.cs
@@ -0,0 +1,33 @@
+﻿public class Student
+{
+    public string Jmeno { get; set; }
+    public int Vek { get; set; }
+    public string Trida { get; set; }
+
+    public List<int> Znamky;
+
+
+    public Student(string jmeno, int vek, string trida)
+    {
+        Jmeno = jmeno;
+        Vek = vek;
+        Trida = trida;
+        Znamky = new List<int>();
+
+    }
+    public void PridatZnamky(int novaZnamka)
+    {
+        Znamky.Add(novaZnamka);
+    }
+    public void VypisZnamky()
+    {
+        Console.WriteLine("Znamky studenta:");
+        foreach (int z in Znamky)
+        {
+            Console.WriteLine("    " + z);
+
+        }
+    }
+}
+
+
